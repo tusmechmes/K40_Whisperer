81c4ac6aa66f1d9dff3d4039da44f30079e70eed
Omer Rosenbaum <omer@synthiverse.com>
2017-09-21 16:36:09 -0700

    clone of K40_Whisperer version 0.10 - using this as a starting base for my updates

---
+++ build_exe.bat
@@ -0,0 +1,15 @@
+rem ---------------------------------------------------------------------
+rem This file executes the build command for the windows executable file.
+rem It is here because I am lazy
+rem ---------------------------------------------------------------------
+del *.pyc
+C:\Python27\python.exe py2exe_setup.py py2exe
+rmdir /S /Q build
+move dist dist32
+pause
+
+del *.pyc
+C:\Python27_64bit\python.exe py2exe_setup.py py2exe
+rmdir /S /Q build
+move dist dist64
+pause
